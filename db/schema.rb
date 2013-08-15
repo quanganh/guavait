d2d268710a6d48bdebdef734f4acc4833d5647b9
Quang Anh <hoquanganh.bmt@gmail.com>
2013-08-15 21:17:21 +0700

    Drop table Posts. Fixes #4

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130809184219) do
+ActiveRecord::Schema.define(version: 20130815141212) do
 
   create_table "contact_messages", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.string   "phone"
     t.text     "message"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "posts", force: true do |t|
-    t.string   "title"
-    t.string   "category"
-    t.text     "content"
-    t.boolean  "active"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
